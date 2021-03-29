f29887d76e63802676cb7fca2daa8e120404367b
Carsten Bauer <crstnbr@gmail.com>
2021-03-30 01:34:19 +0200

    initial

--- src/HwlocViz.jl
+++ src/HwlocViz.jl
@@ -1,5 +1,24 @@
 module HwlocViz
 
-# Write your package code here.
+using Hwloc, Plots, AbstractTrees, GraphRecipes
+
+const topo = topology_load()
+
+AbstractTrees.children(x::Hwloc.Object) = x.children
+AbstractTrees.printnode(io::IO, x::Hwloc.Object) = x.type_ in (:Core, :PU) ? print(io, "$(x.type_) $(x.logical_index)") : print(io, x.type_)
+
+plot_topology(; kwargs...) = begin
+    return plot(TreePlot(topo), fontsize=10, nodeshape=:circle, nodecolor=:white, size=(800,800), kwargs...)
+end
+
+plot_topology(filename::String; kwargs...) = begin
+    p = plot_topology(; kwargs...)
+    savefig(p, filename)
+    return nothing
+end
+
+print_topology() = print_tree(topo, maxdepth=12)
+
+export plot_topology, print_topology
 
 end
